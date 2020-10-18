3c07bc75479cd51ce5282725da932230ec23c975
yuya.shishido <yuya7568@gmail.com>
2020-10-18 17:25:42 +0900

    Add pagination

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -35,12 +35,27 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
+        // 開くページ数を取得（デフォルトは1ページ目）
+        int page = 1;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(NumberFormatException e) {}
+
+        // 最大件数と開始位置を指定してメッセージを取得
         List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class)
+                                   .setFirstResult(15 * (page - 1))
+                                   .setMaxResults(15)
                                    .getResultList();
+
+        // 全件数を取得
+        long tasks_count = (long)em.createNamedQuery("getTasksCount", Long.class)
+                                      .getSingleResult();
 
         em.close();
 
         request.setAttribute("tasks", tasks);
+        request.setAttribute("tasks_count", tasks_count);
+        request.setAttribute("page", page);
 
         // フラッシュメッセージがセッションスコープにセットされていたら
         // リクエストスコープに保存する（セッションスコープからは削除）
